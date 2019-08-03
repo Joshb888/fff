ee7383bae664d5e1c7cad3ee6b2cdaa408fd5628
unknown <codeleap@outlook.com>
2019-08-03 12:25:45 +0200

    Main JS file with employee objects added.

---
+++ main.js
@@ -0,0 +1,7 @@
+let employees = [
+    {firstName:'Brian', lastName:'Wilson', job:'Developer'},
+    {firstName:'Terry', lastName:'Jackson', job:'Manager'},
+    {firstName:'Sally', lastName:'Phelan', job:'Director'},
+    {firstName:'Claire', lastName:'Wilson', job:'Designer'},
+    {firstName:'Debbie', lastName:'Jones', job:'Designer'}
+];
